7bbeb3f946d47fe6a23d381887dfb1c3c0890d38
juli-anne <julianne.lazar@gmail.com>
2025-08-24 16:19:14 +0200

    fix simple test not passing - missing react plugin

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,6 +1,9 @@
 import { render } from '@testing-library/react';
-import Page from '@/app/page';
+import { test, expect } from 'vitest';
+import Page from './page';
+import React from 'react';
 
 test('renders without crashing', () => {
-  render(<Page />);
+  render(React.createElement(Page));
+  expect(true).toBe(true);
 });
